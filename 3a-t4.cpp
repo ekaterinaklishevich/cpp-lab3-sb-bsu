24de854b971ee9ba92199dd6344bb806b77a0b2a
ekaterinaklishevich <114685831+ekaterinaklishevich@users.noreply.github.com>
2022-12-21 18:03:13 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,15 +1,20 @@
-#include<iostream> 
-using namespace std; 
+#include <iostream> 
+using namespace std;
 
-int main()
+int main ()
 {
-int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin >> k;
-do {
-cout << "Enter integer nr. " << i << ": "; 
-cin >> number;
-sum_k += number; i++;
-} while (i <= k);
-cout << " The total sum of " << k << " integers is: "  << sum_k;
-return 0;
-}
+    int k, i = 1, sum_k = 0, number;
+    cout << " Enter the amount of integers to sum = "; 
+    cin >> k;
+    if (k > 0)
+    {
+        do {
+        cout << "Enter integer nr." << i << ": ";
+        cin >> number;
+        sum_k += number; i++;
+        } while (i <= k);
+        cout << " The total sum of " << k << " integers is: " << sum_k;
+    }
+    else cout << " Please enter another value";
+    return 0;
+} 
